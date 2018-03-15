1991027811e8aaabffb12f0a1dff1cb556127ec2
inanfatih <inanfatih@users.noreply.github.com>
2018-03-15 13:42:24 -0400

    UI is created and todos can be captured

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,61 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController,AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
+  public todos = [];
+  public todoDetails = [];
+  public todoDates = [];
+  constructor(public navCtrl: NavController, private alertController: AlertController) {
 
   }
+
+  openTodoAlert(){
+    let addTodoAlert = this.alertController.create({
+      title: "Add A Task",
+      subTitle: "Enter the task, its details and the deadline",
+      inputs: [
+        {
+        type: "text",
+        placeholder: "Enter task",
+        name: "addTodoInput"
+        },
+        {
+          type: "text",
+          placeholder: "Enter details",
+          name:"addTodoDescription"
+        },
+        {
+          type: "date",
+          placeholder: "Enter deadline",
+          name: "addTodoDeadline"
+        }
+      ],
+      buttons: [
+        {
+          text: "Cancel"
+        },
+        {
+          text: "Add Task",
+          handler: (inputData)=>{
+            let todoText, todoDescription, todoDate;
+            todoText = inputData.addTodoInput;
+            todoDescription = inputData.addTodoDescription;
+            todoDate = inputData.addTodoDeadline;
+            this.todos.push(todoText);
+            this.todoDetails.push(todoDescription);
+            this.todoDates.push(todoDate);
+
+          }
+        }
+      ]
+    });
+    addTodoAlert.present();
+
+}
+
 
 }
